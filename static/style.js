24afab1575f325dc58516dbd02bf217deda19030
Audrey <Laparticule@users.noreply.github.com>
2017-08-20 15:55:55 +0200

    Create style.js

---
+++ static/style.js
@@ -0,0 +1,9 @@
+document.getElementById("leftbar").onclick = function() {
+	if (document.getElementById("menu").style.display == "inline") {
+		document.getElementById("menu").style.display = "none";
+	}
+	else {
+		document.getElementById("menu").style.display = "inline";
+	}
+}
+
